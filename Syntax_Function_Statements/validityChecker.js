27170a22cf0aff95d9f7557db8b126c5db299d58
Dimitar Dimitrov <66394357+DimAntDim@users.noreply.github.com>
2022-02-04 20:25:19 -0600

    validityChecker fix output lines

--- Syntax_Function_Statements/validityChecker.js
+++ Syntax_Function_Statements/validityChecker.js
@@ -4,9 +4,9 @@ function validityChecker(x1, y1, x2, y2) {
         
         return (Number.isInteger(result)) ? 'valid' : 'invalid'
     }
-    return `{${x1}, ${y1}} to {0, 0} is ${getResult(x1, y1, 0, 0)}\n
-{${x2}, ${y2}} to {0, 0} is ${getResult(x2, y2, 0, 0)}\n
-{${x1}, ${y1}} to {${x2}, ${y2}} is ${getResult(x1, y1, x2, y2)}`
+    return `{${x1}, ${y1}} to {0, 0} is ${getResult(x1, y1, 0, 0)}\n`+
+`{${x2}, ${y2}} to {0, 0} is ${getResult(x2, y2, 0, 0)}\n`+
+`{${x1}, ${y1}} to {${x2}, ${y2}} is ${getResult(x1, y1, x2, y2)}`
 }
 
 console.log(validityChecker(3, 0, 0, 4))
