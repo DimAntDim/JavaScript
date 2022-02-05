954f0b9fb7cca0bfda86977b4006fd4cc1c283bf
Dimitar Dimitrov <66394357+DimAntDim@users.noreply.github.com>
2022-02-04 20:18:20 -0600

    validity checker

--- Syntax_Function_Statements/validityChecker.js
+++ Syntax_Function_Statements/validityChecker.js
@@ -1,15 +1,12 @@
 function validityChecker(x1, y1, x2, y2) {
-    let base_x = 0
-    let base_y = 0
-    let check1 = Math.sqrt(Math.pow(x1-base_x)+Math.pow(y1-base_y))
-    let check2 = Math.sqrt(Math.pow(x2-base_x)+Math.pow(y2-base_y))
-    let check3 = Math.sqrt(Math.pow(x1-x2)+Math.pow(y1-y2))
-    if (check1.isinteger) {
-        return `{x1, y1} to {x2, y2} is valid`;
+    function getResult(x1, y1, x2, y2) {
+        let result = Math.sqrt((x2-x1)**2 + (y2-y1)**2)
+        
+        return (Number.isInteger(result)) ? 'valid' : 'invalid'
     }
-    else { 
-        return `${x1, y1} to ${x2, y2} is invalid`;
-    }    
+    return `{${x1}, ${y1}} to {0, 0} is ${getResult(x1, y1, 0, 0)}\n
+{${x2}, ${y2}} to {0, 0} is ${getResult(x2, y2, 0, 0)}\n
+{${x1}, ${y1}} to {${x2}, ${y2}} is ${getResult(x1, y1, x2, y2)}`
 }
 
 console.log(validityChecker(3, 0, 0, 4))
